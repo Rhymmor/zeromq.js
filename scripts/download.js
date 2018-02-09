ba6b7f8b06b6cc5b4c432e44f387a446b6397046
Anton Kiselev <anton.kiselev@xored.com>
2018-02-09 17:06:20 +0700

    use curl for downloads

--- scripts/download.js
+++ scripts/download.js
@@ -1,42 +1,10 @@
-var https = require("https");
-var fs = require("fs");
-var url = require("url");
-
-function writeToFile(filename, response, callback) {
-  response.pipe(fs.createWriteStream(filename));
-  response.on("end", callback);
-}
+var child_process = require("child_process");
 
 function download(fileUrl, filename, callback) {
-  https
-    .get(fileUrl, function(response) {
-      if (
-        response.statusCode > 300 &&
-        response.statusCode < 400 &&
-        response.headers.location
-      ) {
-        if (url.parse(response.headers.location).hostname) {
-          https.get(response.headers.location, function(res) {
-            writeToFile(filename, res, callback);
-          });
-        } else {
-          https
-            .get(
-              url.resolve(
-                url.parse(fileUrl).hostname,
-                response.headers.location
-              ),
-              function(res) {
-                writeToFile(filename, res, callback);
-              }
-            )
-            .on("error", callback);
-        }
-      } else {
-        writeToFile(filename, response, callback);
-      }
-    })
-    .on("error", callback);
+  child_process.spawn('curl', ['-L', '-o', filename, fileUrl], {stdio: 'inherit'})
+      .on('close', function(code, signal) {
+          callback(code === 0 ? undefined : 'Download failed');
+      });
 }
 
 module.exports = {
